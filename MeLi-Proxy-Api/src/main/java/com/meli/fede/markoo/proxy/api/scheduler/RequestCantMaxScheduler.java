a0bc526c29587d76622f7b74a8142615f3e74171
Federico Gomez Markowicz <ARX0001098>
2021-11-18 10:59:33 -0300

    [BUGFIX] si no hay registros en la bd rompia al validar los maxs

--- MeLi-Proxy-Api/src/main/java/com/meli/fede/markoo/proxy/api/scheduler/RequestCantMaxScheduler.java
+++ MeLi-Proxy-Api/src/main/java/com/meli/fede/markoo/proxy/api/scheduler/RequestCantMaxScheduler.java
@@ -3,6 +3,7 @@ package com.meli.fede.markoo.proxy.api.scheduler;
 import com.meli.fede.markoo.proxy.api.data.repository.MongoRepository;
 import com.meli.fede.markoo.proxy.api.values.AccessManagerValues;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,14 +15,32 @@ public class RequestCantMaxScheduler {
 
     private final MongoRepository repository;
     private final AccessManagerValues value;
+    @Value("${com.meli.fede.markoo.proxy.defaultMax}")
+    private Integer defaultMax;
 
     @Scheduled(initialDelay = 0, fixedDelayString = "${com.meli.fede.markoo.proxy.refresh-cant-delay}")
     public void maxRequestCant() {
-        final AccessManagerValues accessManagerValues = this.repository.getMaxCantRequest();
-        this.value.setMaxRequestPerIp(accessManagerValues.getMaxRequestPerIp());
-        this.value.setMaxRequestPerPath(accessManagerValues.getMaxRequestPerPath());
-        this.value.setMaxRequestPerCombo(accessManagerValues.getMaxRequestPerCombo());
-        this.value.setMaxRequestPerUserAgent(accessManagerValues.getMaxRequestPerUserAgent());
+        AccessManagerValues accessManagerValues = this.repository.getMaxCantRequest();
+        if (accessManagerValues == null) {
+            accessManagerValues = new AccessManagerValues();
+            accessManagerValues.setMaxRequestPerIp(this.defaultMax);
+            accessManagerValues.setMaxRequestPerPath(this.defaultMax);
+            accessManagerValues.setMaxRequestPerCombo(this.defaultMax);
+            accessManagerValues.setMaxRequestPerUserAgent(this.defaultMax);
+        }
+        final Integer maxRequestPerIp = accessManagerValues.getMaxRequestPerIp();
+        final Integer maxRequestPerPath = accessManagerValues.getMaxRequestPerPath();
+        final Integer maxRequestPerCombo = accessManagerValues.getMaxRequestPerCombo();
+        final Integer maxRequestPerUserAgent = accessManagerValues.getMaxRequestPerUserAgent();
+
+        this.value.setMaxRequestPerIp(RequestCantMaxScheduler.getNormalizedValue(maxRequestPerIp));
+        this.value.setMaxRequestPerPath(RequestCantMaxScheduler.getNormalizedValue(maxRequestPerPath));
+        this.value.setMaxRequestPerCombo(RequestCantMaxScheduler.getNormalizedValue(maxRequestPerCombo));
+        this.value.setMaxRequestPerUserAgent(RequestCantMaxScheduler.getNormalizedValue(maxRequestPerUserAgent));
+    }
+
+    private static int getNormalizedValue(final Integer value) {
+        return value == null || value < 0 ? 0 : value;
     }
 }
 
